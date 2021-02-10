7748c91f522de52cd851837845e244767aed0080
Andi Irham <Andi.irhamm@gmail.com>
2021-02-10 19:54:31 +0700

    Update native-lib.cpp
    Shadowed API key

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,7 @@ JNIEXPORT jstring JNICALL
 Java_id_airham_moviecatalogue_utils_Keys_getApiKey(
         JNIEnv *env,
         jobject /* thiz */) {
-    std::string key = "921583536fd3e09b74a1cdac13e87f52";
+        // GET YOUR APIKEY FROM https://www.themoviedb.org/
+    std::string key = "YOUR MOVIEDB APIKEY";
     return env->NewStringUTF(key.c_str());
 }
